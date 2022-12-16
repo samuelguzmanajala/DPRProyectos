c576320e7add4bcd3dea27b6323655476bfa24b4
samuel <samuel@pop-os.localdomain>
2022-12-16 17:39:24 +0100

    primera parte

--- Topology.py
+++ Topology.py
@@ -3,15 +3,15 @@ import sys
 class Topology( Topo ):
     def __init__(self):
         Topo.__init__( self )
-        fo = 2
-        Ts = self.addSwitch('Ts', dpid = '0000000100002022')
-        if fo<1 or fo >255:
-            print('rango incorrecto, debe estar entre 1 y 255')
-        else:
-            for i in range(1, fo+1):
-                switches = self.addSwitch('s' + str(i))
-                self.addLink(switches, Ts, 1, i)
-                for j in range(1, fo+1):
-                    hosts = self.addHost('h_s'+ str(i) + 'n' + str(j), ip = '10.0.' + str(i)+ '.' + str(j) + '/16')
-                    self.addLink(hosts, switches, 1, j+1)
+        fo = input('cuantos switchs?')
+        if fo<1 or fo >255: #Lanzamos una excepcion si el valor fo es incorrecto
+            raise AttributeError
+        print("se crearan", fo, "switches y", fo*fo, "hosts")
+        parentSwitch = self.addSwitch('Ts', dpid = '0000000000002022')
+        for i in range(1, fo+1):
+            switches = self.addSwitch('s' + str(i))
+            self.addLink(switches, parentSwitch, 1, i)
+            for j in range(1, fo+1):
+                hosts = self.addHost('host_'+ str(i) + '_' + str(j), ip = '10.0.' + str(i)+ '.' + str(j) + '/16')
+                self.addLink(hosts, switches, 1, j+1)
 topos = {'topology': (lambda:Topology())}
